b4dd089ab9d8bb44ce6aec2baeafe4f663958ae8
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-06-27 17:43:48 +0300

    Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,23 @@
 import Profile from "./Profile/Profile";
+import Statistics from "./Statistics/Statistics";
 
 import user from 'path/to/user.json';
+import data from 'path/to/data.json';
 
 export const App = () => {
   return (
     <div>
     <Profile
-    name={user.username}
-    tag={user.tag}
-    location={user.location}
-    avatar={user.avatar}
-    stats={user.stats}
-  />
+       name={user.username}
+       tag={user.tag}
+       location={user.location}
+       avatar={user.avatar}
+       stats={user.stats}
+    />
+    <Statistics
+      title
+      stats={data}
+    />
     </div>
   );
 };
