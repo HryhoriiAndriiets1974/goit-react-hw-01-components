6edf6bed63293e42257b777556fc891d6d647662
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-06-28 18:26:00 +0300

    error-const

--- src/path/getRandomColor.js
+++ src/path/getRandomColor.js
@@ -1,7 +1,7 @@
 export default function getRandomColor() {
-  const r = Math.floor(Math.random() * 255);
-  const g = Math.floor(Math.random() * 255);
-  const b = Math.floor(Math.random() * 255);
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
   if (r === 255 && g === 255 && b === 255) {
     r = 0;
     g = 0;
