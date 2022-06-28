ed5a856be8ac9aabd01fa795fcf604c4926e744c
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-06-28 18:13:32 +0300

    css-for-TransactionHistory

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
 
 export default function TransactionHistory({items}) {
   return (
-  <table className="transaction-history">
-    <thead>
+  <table className={css.transactionHistory}>
+    <thead className={css.thead}>
       <tr>
         <th>Type</th>
         <th>Amount</th>
@@ -13,10 +14,12 @@ export default function TransactionHistory({items}) {
 
     <tbody>
       {items.map(item => (
-        <tr key={ item.id }>
-          <td>{ item.type }</td>
-          <td>{ item.amount }</td>
-          <td>{ item.currency }</td>
+        <tr
+          className={css.item}
+          key={ item.id }>
+            <td>{ item.type }</td>
+            <td>{ item.amount }</td>
+            <td>{ item.currency }</td>
         </tr>
       ))}
     </tbody>
